a4a042b17e6cf1b3818f93d33829808f00eb3a4e
Đức Nguyễn Đăng <duc.nd5@shbfinance.com.vn>
2022-02-09 14:40:43 +0700

    Initial Commit

---
+++ src/main/java/com/ndangducbn/aop/AOPSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.ndangducbn.aop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AOPSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AOPSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ndangducbn/aop/AOPSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ndangducbn.aop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AOPSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
